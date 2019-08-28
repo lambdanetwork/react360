639e810a5a12aaf7249581771e69ef969ae8bdb3
vidy.alfredo@shopee.com <vidy.alfredo@shopee.com>
2019-08-28 22:37:54 +0700

    change manifest

--- client.js
+++ client.js
@@ -1,128 +1,40 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance, Module, Surface} from 'react-360-web';
+import { initTourism } from './components/surfaceModules/SurfaceTourism' 
+import { initShopeeOffice } from './components/surfaceModules/kantorTourism';
 
-function init(bundle, parent, options = {}) {
-  r360 = new ReactInstance(bundle, parent, {
-    // Add custom options here
-    fullScreen: true,
-    nativeModules: [
-      new surfaceModule(),
-    ],
-    assetRoot: 'https://cf.shopee.co.id/file/',
-    ...options,
-  });
 
-  introPanel = new Surface(
-    500, /* width */
-    400, /* height */
-    Surface.SurfaceShape.Cylinder /* shape */
-  );
+function run (){
+  if(window.location.search.indexOf('office') >= 0){
+    window.React360 = {init: initShopeeOffice};
+    window.React360.init(
+      'index.bundle?platform=vr&dev=true',
+      document.getElementById('container'),
+      {
+        assetRoot: 'static_assets/',
+      })
+      setTimeout(() => {
+        Array.from(document.getElementsByTagName('canvas'))
+        .forEach(canvas => {
+          const width = canvas.style.width.replace('px','');
+          const height = canvas.style.height.replace('px','');
+          canvas.style = `width: ${Number(width)*1.5}; height: ${Number(height)*1.5};`
+          console.log(canvas)
 
-  introRoot = r360.renderToSurface(
-    r360.createRoot('TourismVR', { /* initial props */ }),
-    introPanel
-  );
-
-  marketPanel = new Surface(
-    100,
-    100,
-    Surface.SurfaceShape.Flat
-  )
-
-  marketPanel.setAngle(
-    0.2, /* yaw angle */
-    0 /* pitch angle */
-  );
-
-  museumPanel = new Surface(
-    100,
-    100,
-    Surface.SurfaceShape.Flat
-  )
-
-  museumPanel.setAngle(
-    Math.PI / 2, /* yaw angle */
-    0 /* pitch angle */
-  );
-
-  restaurantPanel = new Surface(
-    100,
-    100,
-    Surface.SurfaceShape.Flat
-  )
-
-  restaurantPanel.setAngle(
-    -Math.PI / 2, /* yaw angle */
-    0 /* pitch angle */
-  );
-
-  shoppingPanel = new Surface(
-    100,
-    100,
-    Surface.SurfaceShape.Flat
-  );
-
-  shoppingPanel.setAngle(
-    3.6, /* yaw angle */
-    0 /* pitch angle */
-  );
-
-  // Load the initial environment
-  r360.compositor.setBackground('https://cf.shopee.co.id/file/05b33fb9783a13d6d0f8784cffef2d35');
-
+        })
+      },1000)
+    
+  } else {
+    window.React360 = {init: initTourism};
+ window.React360.init(
+    'index.bundle?platform=vr&dev=true',
+    document.getElementById('container'),
+    {
+      assetRoot: 'static_assets/',
+    })
+  }
+ 
 }
 
-class surfaceModule extends Module {
-  constructor() {
-    super('surfaceModule');
-  }
-
-  resizeSurface(width, height, id) {
-    if (id === 'museum') {
-      museumPanel.resize(width, height);
-    } else if (id === 'restaurant') {
-      restaurantPanel.resize(width, height);
-    } else if (id === 'shopping') {
-      shoppingPanel.resize(width, height);
-    } else if (id === 'market') {
-      marketPanel.resize(width, height);
-    }
-  }
-
-  start() {
-    r360.renderToSurface(
-      r360.createRoot('InfoPanel', { id: 'market',
-                                     text: 'Browse our incredible market.',
-                                     image: '' }),
-      marketPanel,
-    );
-
-    r360.renderToSurface(
-      r360.createRoot('InfoPanel', { id: 'shopping',
-                                     text: 'Shop until you drop!',
-                                     image: ''}),
-      shoppingPanel,
-    );
-
-    r360.renderToSurface(
-      r360.createRoot('InfoPanel', { id: 'museum',
-                                     text: 'The Life of Pablo Picasso: Blue.', 
-                                     image: ''}),
-      museumPanel,
-    );
-
-    r360.renderToSurface(
-      r360.createRoot('InfoPanel', { id: 'restaurant',
-                                     text: 'Enjoy a delicious beer at our restaurants.',
-                                     image: '' }),
-      restaurantPanel,
-    );
-
-    r360.detachRoot(introRoot);
-  }
-}
-
-
-window.React360 = {init};
+run();
---
+++ components/componentModules/TourismVR.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  Image,
+  NativeModules,
+  StyleSheet,
+  Text,
+  View,
+  VrButton
+} from 'react-360';
+
+import {Surface} from 'react-360-web';
+
+const surfaceModule = NativeModules.surfaceModule;
+const imagesObj = {
+  info: {
+    name: 'info',
+    src: 'https://cf.shopee.co.id/file/a45b11e0a5bc3b45c1ba001b32b156ff',
+    width: 100,
+    height: 100
+  },
+  market: {
+    name: 'market',
+    src: 'https://cf.shopee.co.id/file/1a5d88d090bf46fd0c06ebe18102dfb6',
+    width: 500,
+    height: 300
+  },
+  museum: {
+    name: 'museum',
+    src: 'https://cf.shopee.co.id/file/90eac735618117954c9d25e3481f33cc',
+    width: 500,
+    height: 300
+  },
+  restaurant: {
+    name: 'restaurant',
+    src: 'https://cf.shopee.co.id/file/52f26dbfc83b0da9eea9b45cf19120d0',
+    width: 500,
+    height: 300
+  },
+  shopping: {
+    name: 'shopping',
+    src: 'https://cf.shopee.co.id/file/43e2825b776c9fb57289825eaf3adab1',
+    width: 500,
+    height: 300
+  }
+}
+
+// NativeModules.ExternalAssets.assetRoot = 'https://cf.shopee.co.id/file/';
+
+export class InfoPanel extends React.Component {
+  state = {
+    img: imagesObj.info,
+  }
+
+  transformDisplay(id) {
+    if(this.state.img.name.indexOf(id) >= 0){
+      this.resetPanel(id)
+      return 
+    }
+    this._changeSurfaceDimensions(500, 400, id);
+    this.setState({img: imagesObj[id] });
+  }
+
+  resetPanel(id) {
+    this._changeSurfaceDimensions(100, 100, id);
+    this.setState({img: imagesObj.info});
+  }
+
+  _changeSurfaceDimensions(width, height, id) {
+    surfaceModule.resizeSurface(width, height, id);
+  }
+
+  render() {
+    let { img } = this.state;
+    return (
+      <View style={styles.displayPanel}
+            hitSlop={20}
+            >
+            <VrButton  onClick={() => this.transformDisplay(this.props.id)}>
+              <Image source={{uri: img.src}} style={{width: img.width, height: img.height}} />
+              <View style={styles.messageBox}>
+                <Text style={styles.attractionText}>
+                  {this.props.text}
+                </Text>
+              </View>
+            </VrButton>
+      </View>
+    );
+  }
+};
+
+
+export default class TourismVR extends React.Component {
+  render() {
+    return (
+      <View>
+        <View style={styles.attractionBox}>
+          <VrButton onClick={() => surfaceModule.start()}>
+            <Text style={styles.attractionText}>
+              Welcome!
+            </Text>
+            <Text  style={styles.attractionText}>
+              Simple VR with React 360
+            </Text>
+            <Text  style={styles.attractionText}>
+              Click Me to Start
+            </Text>
+          </VrButton>
+        </View>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  displayPanel: {
+    width: 100,
+    height: 100,
+    flexDirection: 'column',
+  },
+  messageBox: {
+    padding: 20,
+    backgroundColor: '#F7F7F7',
+    borderColor: '#C4002F',
+    borderWidth: 2,
+    width: 500,
+  },
+  attractionBox: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#F7F7F7',
+    borderColor: '#C4002F',
+    borderWidth: 2,
+    width: 500,
+    height: 300,
+  },
+  attractionText: {
+    fontSize: 30,
+    color: '#C4002F'
+  },
+});
